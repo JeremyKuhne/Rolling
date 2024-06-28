1261dbead6deee680730513f9a6fc7b0170f6c12
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-06-28 10:34:36 -0700

    Add project files.

---
+++ Current/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Current
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+            BinaryFormatter formatter = new();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+            MemoryStream stream = new();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+            formatter.Serialize(stream, "Hello, World!");
+            stream.Position = 0;
+            Console.WriteLine(formatter.Deserialize(stream));
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+        }
+    }
+}
---
+++ Rolling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Rolling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryFormatter formatter = new();
+            MemoryStream stream = new();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+            formatter.Serialize(stream, "Hello, World!");
+            stream.Position = 0;
+            Console.WriteLine(formatter.Deserialize(stream));
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+        }
+    }
+}
